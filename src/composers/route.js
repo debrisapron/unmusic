766fe1f6b1ed070f8fa5450edb261e4fbe02af44
DEBRIS APRON <debrisapron@gmail.com>
2016-12-13 17:33:05 -0800

    Simplify route

--- src/composers/route.js
+++ src/composers/route.js
@@ -1,21 +1,8 @@
 let _ = require('lodash/fp')
+let seq = require('./seq')
 
-let route = (...args) => {
-  return args.reverse().reduce(connectPair)
-}
-
-let connectPair = (dest, src) => {
-  if (src.actions) {
-    return setScoreDest(dest, src)
-  }
-  if (src.connect) {
-    src.connect(dest)
-    return src
-  }
-  throw new Error('You tried to route from an object that neither is a score nor has a connect method')
-}
-
-let setScoreDest = (dest, score) => {
+let route = (dest, ...args) => {
+  let score = seq(...args)
   let actions = score.actions.map((a) => {
     return a.type === 'NOOP' ? a : _.set('payload.dest', dest, a)
   })
@@ -28,31 +15,15 @@ module.exports = route
 
 if (!process.env.TEST) return
 
-test('Can route connectables to each other returning first', (assert) => {
-  let conns = {}
-  let data
-  let c1 = { connect: (dest) => conns.c1 = dest }
-  let c2 = { connect: (dest) => conns.c2 = dest }
-  let c3 = { connect: (dest) => conns.c3 = dest }
-  let c4 = route(c1, c2, c3)
-  assert.deepEqual(conns, { c1: c2, c2: c3 })
-  assert.equal(c1, c4)
-  assert.end()
-})
-
-test('Can route score to connectables returning a new event list', (assert) => {
-  let conns = {}
+test('can route score to dest', (assert) => {
   let s1 = { actions: [
     { type: 'NOTE', payload: { time: 0,   nn: 69, dur: 1/4 } },
     { type: 'NOTE', payload: { time: 3/8, nn: 69, dur: 1/4 } },
     { type: 'NOOP', payload: { time: 1 } }
   ] }
-  let c2 = { connect: (dest) => conns.c2 = dest, name: 'c2' }
-  let c3 = { connect: (dest) => conns.c3 = dest, name: 'c3' }
-  let s2 = route(s1, c2, c3)
-  assert.deepEqual(conns, { c2: c3 })
-  assert.equal(s2.actions[0].payload.dest, c2)
-  assert.equal(s2.actions[1].payload.dest, c2)
+  let s2 = route('foo', s1)
+  assert.equal(s2.actions[0].payload.dest, 'foo')
+  assert.equal(s2.actions[1].payload.dest, 'foo')
   assert.false(s2.actions[2].payload.dest)
   assert.end()
 })
