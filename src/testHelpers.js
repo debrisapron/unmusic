b6cdf38cf918563ab6ba3495fd68f9693ad2bcaa
DEBRIS APRON <debrisapron@gmail.com>
2017-05-07 17:54:05 +0000

    checkpoint

--- src/testHelpers.js
+++ src/testHelpers.js
@@ -1,7 +1,16 @@
+let _ = require('lodash/fp')
 let diff = require('deep-diff').diff
 
 let deepMatches = (actual, expected) => {
   return diff(actual, expected).every((change) => change.kind === 'D')
 }
 
-module.exports = { deepMatches }
+let log = (data) => console.log(toLogString(data))
+
+let toLogString = (data) => {
+  if (_.isString(data)) return `String: ${ data }`
+  if (_.isNumber(data)) return `Number: ${ data }`
+  return '\n' + JSON.stringify(data, null, 2)
+}
+
+module.exports = { deepMatches, log }
