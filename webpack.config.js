86cea9fd1194da1219d010e08d54a1fde1379bb4
DEBRIS APRON <debrisapron@gmail.com>
2018-02-14 13:07:39 -0800

    Add dist bundle with webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+let path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'unmusic.js',
+    library: 'Unmusic',
+    libraryTarget: "umd"
+  }
+}
