1234b8c54e98fa0de8436cd12cdd6b63db039c23
DEBRIS APRON <debrisapron@gmail.com>
2018-10-14 12:58:55 -0700

    Fix sound of test song

--- newSong.um.js
+++ newSong.um.js
@@ -1,7 +1,36 @@
 module.exports = (um) => {
-  let {
-    arpeg, arrange, config, flow, loop, mix, offset, seq, tempo, tran, orch
-  } = um
 
-  return seq('kick')
+  let orch = um.orch(`
+    instr hihat
+    aamp      expon     1,  0.1,   0.01
+    arand     rand      aamp
+    outs arand*0.5, arand*0.5
+    endin
+
+    instr snare
+    aenv1  expon  1, 0.03, 0.5
+    a1   oscili aenv1, 147
+    aamp      expon     1,  0.2,   0.01
+    arand     rand      aamp
+    outs a1+(arand*0.5), a1+(arand*0.5)
+    endin
+
+    instr kick
+    k1  expon    100, .2, 50
+    aenv expon 1, 0.15, 0.01
+    a1  poscil    1, k1
+    outs a1*aenv, a1*aenv
+    endin`
+  )
+
+  return um.flow(
+    um.mix(
+      '/8 kick  kick  kick  kick ',
+      '/8 _     _     snare _    ',
+      '/8 hihat hihat hihat hihat'
+    ),
+    um.tempo(140),
+    um.loop,
+    orch
+  )
 }
