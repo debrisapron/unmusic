334b7ecac57277c22d249ce79af5bd8bbf054354
DEBRIS APRON <debrisapron@gmail.com>
2018-10-24 14:29:47 -0700

    Fix initial editor display

--- src/ui/Editor.js
+++ src/ui/Editor.js
@@ -1,40 +1,50 @@
 import React from "react";
 import MonacoEditor from "react-monaco-editor";
 
+const EDITOR_OPTIONS = {
+  selectOnLineNumbers: true,
+  wordWrap: "wordWrapColumn",
+  formatOnType: true,
+  wordWrapColumn: 80,
+  rulers: [80],
+  lineNumbers: "on"
+};
+
 class Editor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: "// type your code..."
+      code: "// type your code...",
+      width: 0,
+      height: 0
     };
-    this.myRef = React.createRef();
   }
+
   editorDidMount(editor, monaco) {
     console.log("editorDidMount", editor);
     editor.focus();
   }
+
   onChange(newValue, e) {
     console.log("onChange", newValue, e);
   }
+
+  setDimensions = el => {
+    const { width, height } = el;
+    this.setState({ width, height });
+  };
+
   render() {
-    const code = this.state.code;
-    const options = {
-      selectOnLineNumbers: true
-    };
+    const { code, width, height } = this.state;
     return (
-      <div style={{ width: "100%", height: "100%" }} ref={this.myRef}>
+      <div style={{ width: "100%", height: "100%" }} ref={this.setDimensions}>
         <MonacoEditor
-          width={this.myRef.current.width}
-          height={this.myRef.current.height}
+          width={width}
+          height={height}
           language="javascript"
-          lineNumbers="on"
           theme="vs-dark"
-          wordWrap="wordWrapColumn"
-          formatOnType={true}
-          wordWrapColumn={80}
-          rulers={[80]}
           value={code}
-          options={options}
+          options={EDITOR_OPTIONS}
           onChange={this.onChange}
           editorDidMount={this.editorDidMount}
         />
